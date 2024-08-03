328e4dd81000f6a604600a4a1c22c2f08b1dd77b
simon <simonwubshet254@gmail.com>
2024-08-03 13:35:19 +0300

    Read data from db using API

---
+++ customer/read.php
@@ -0,0 +1,28 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json'); 
+header('Access-Control-Allow-Methods: GET'); 
+header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
+
+
+include('fuction.php');
+
+$requestMethod = $_SERVER["REQUEST_METHOD"];
+
+if($requestMethod == "GET"){
+$customerList= getCustomerList();
+echo $customerList;
+
+}else{
+
+    $data =[
+        'satus'=> 405,
+        'message' => $requestMethod. 'Method not allowed',
+    ];
+    header("HTTP/1.0 405 METHOD NOT allowed");
+    echo json_encode($data);
+}
+
+
+
+?>
---
+++ inc/data.php
@@ -0,0 +1,8 @@
+<?php
+$conn=mysqli_connect("localhost:3306","root","","phptest");
+if(!$conn){
+    die("connection faild:". mysqli_connect_error());
+
+}
+
+?>
